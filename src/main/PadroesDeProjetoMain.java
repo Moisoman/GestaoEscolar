f0474282f1c6f681405bbdedf28a3c6ae22699f3
Moisés Pedro <moises.monteiro@unioeste.br>
2025-11-13 08:27:13 -0300

    Correção no observer

--- src/main/PadroesDeProjetoMain.java
+++ src/main/PadroesDeProjetoMain.java
@@ -7,45 +7,7 @@ import org.example.alunos.Observer;
 
 public class PadroesDeProjetoMain {
     public static void main(String[] args) {
-        // Demonstrando o padrão Observer
-        demonstrarObserver();
-
-//        // Demonstrando o padrão Singleton
-//        demonstrarSingleton();
-//
-//        // Demonstrando o padrão Factory
-//        demonstrarFactory();
-
-        // E assim por diante para os outros padrões...
-    }
-
-    private static void demonstrarObserver() {
-        AlunosObserver alunosObserver = new AlunosObserver();
-
-        Observer aluno1 = new NotificarAlunos("João");
-        Observer aluno2 = new NotificarAlunos("Maria");
-        Observer aluno3 = new NotificarAlunos("Pedro");
-
-        alunosObserver.addObserver(aluno1);
-        alunosObserver.addObserver(aluno2);
-        alunosObserver.addObserver(aluno3);
-
-        alunosObserver.setMensagem("Aulas serão remarcadas para amanhã!");
-
-        alunosObserver.removeObserver(aluno1);
-
-        alunosObserver.setMensagem("Resultado de provas foi publicado!");
-    }
 
     }
-
-//    private static void demonstrarSingleton() {
-//
-//    }
-//
-//    private static void demonstrarFactory() {
-//
-//    }
-
-    // Adicione métodos para cada um dos padrões...
+    }
 
--- src/main/alunos/AlunosObserver.java
+++ src/main/alunos/AlunosObserver.java
@@ -29,4 +29,27 @@ public class AlunosObserver implements Subject {
         this.mensagem = mensagem;
         notifyObservers();
     }
+
+    public static void main(String[] args) {
+        demonstrarObserver();
+
+    }
+
+    private static void demonstrarObserver() {
+        AlunosObserver alunosObserver = new AlunosObserver();
+
+        Observer aluno1 = new NotificarAlunos("João");
+        Observer aluno2 = new NotificarAlunos("Maria");
+        Observer aluno3 = new NotificarAlunos("Pedro");
+
+        alunosObserver.addObserver(aluno1);
+        alunosObserver.addObserver(aluno2);
+        alunosObserver.addObserver(aluno3);
+
+        alunosObserver.setMensagem("Aulas serão remarcadas para amanhã!");
+
+        alunosObserver.removeObserver(aluno1);
+
+        alunosObserver.setMensagem("Resultado de provas foi publicado!");
+    }
 }
