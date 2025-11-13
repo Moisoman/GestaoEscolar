a9d28181ecb5af391d918393bee88a6647016f83
Moisés Pedro <moises.monteiro@unioeste.br>
2025-11-13 11:38:01 -0300

    Proxy Implementado

---
+++ src/main/entities/interfaces/DiretorInterface.java
@@ -0,0 +1,8 @@
+package org.example.entities.interfaces;
+
+
+public interface DiretorInterface {
+    void mostrarInformacoes();
+    void coordenarProfessores();
+    void fazerReuniaoPedagogica();
+}
---
+++ src/main/professores/Proxy/DiretorProxy.java
@@ -0,0 +1,49 @@
+package org.example.professores.Proxy;
+
+import org.example.calendario.TemplateMethod.CalendarioTemplate;
+import org.example.calendario.TemplateMethod.EventoReuniao;
+import org.example.entities.Diretor;
+import org.example.entities.interfaces.DiretorInterface;
+
+/**
+ * Fornece um substituto ou um representante para outro objeto,
+ * controlando o acesso a ele.
+ * Ele pode ser usado para controlar a criação,
+ * o acesso, e a execução de operações em objetos
+ */
+public class DiretorProxy implements DiretorInterface {
+    private Diretor diretor;
+    private String nome;
+    private String escola;
+
+    public DiretorProxy(String nome, String escola) {
+        this.nome = nome;
+        this.escola = escola;
+    }
+
+    private void carregarDiretor() {
+        if (diretor == null) {
+            diretor = new Diretor(nome, escola);
+        }
+    }
+
+    @Override
+    public void mostrarInformacoes() {
+        System.out.println("Acessando informações do diretor...");
+        carregarDiretor();
+        diretor.mostrarInformacoes();
+    }
+
+    @Override
+    public void coordenarProfessores() {
+        carregarDiretor();
+        diretor.coordenarProfessores();
+    }
+
+    @Override
+    public void fazerReuniaoPedagogica() {
+        carregarDiretor();
+        diretor.fazerReuniaoPedagogica();
+    }
+    
+}
